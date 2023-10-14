3e51a5cf169edc7a6124698a6844a2c3081a0a51
carmine.accogli01 <carmine.accogli01@gmail.com>
2023-10-14 16:28:28 +0200

    Implemented Security with JWT token

---
+++ src/main/java/it/unisalento/smartcitywastemanagement/alarmsms/security/SecurityConstants.java
@@ -0,0 +1,7 @@
+package it.unisalento.smartcitywastemanagement.alarmsms.security;
+
+public class SecurityConstants {
+    public static final String JWT_SECRET = "seedseedseedseedseedseedseedseedseedseedseed";
+
+    public static final String THIS_MICROSERVICE ="http://alarmsMS:8085";
+}
