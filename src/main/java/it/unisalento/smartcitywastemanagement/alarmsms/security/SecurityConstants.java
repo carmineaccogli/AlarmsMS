90adfdecc837c82d1d1593d8cbd31d0157a04874
carmine.accogli01 <carmine.accogli01@gmail.com>
2024-01-24 17:56:05 +0100

    .gitignore add element

--- src/main/java/it/unisalento/smartcitywastemanagement/alarmsms/security/SecurityConstants.java
+++
@@ -1,7 +0,0 @@
-package it.unisalento.smartcitywastemanagement.alarmsms.security;
-
-public class SecurityConstants {
-    public static final String JWT_SECRET = "seedseedseedseedseedseedseedseedseedseedseed";
-
-    public static final String THIS_MICROSERVICE ="http://alarmsMS:8085";
-}
